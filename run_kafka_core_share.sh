83ae79f6eb291dc5f6bbed0cabdbc499aa850cd5
stream <penolove15@gmail.com>
2017-05-02 17:29:33 +0800

    add some comment

--- run_kafka_core_share.sh
+++ run_kafka_core_share.sh
@@ -1,2 +1,3 @@
-docker run --network container:spark_core -ti  --name kafka_core \
+#docker run --network container:spark_core -ti  --name kafka_core \
+docker run --network container:hadoop_core -ti  --name kafka_core \
     -d penolove/centos7:kafka_core;
