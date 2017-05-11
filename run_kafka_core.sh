4527e3ba192e7f23b78e1ca17dd9315d7f281cb8
stream <penolove15@gmail.com>
2017-05-11 17:16:09 +0800

    remove some useless file

--- run_kafka_core.sh
+++ run_kafka_core.sh
@@ -1,4 +1,4 @@
-docker run -ti -h invpm28  --name kafka_core \
+docker run -ti -h invpm27  --name kafka_core \
     -v $(pwd)/Docker_kafka_core/start.sh:/root/start.sh \
     -d penolove/centos7:kafka_core;
 sudo ./pipework.sh
